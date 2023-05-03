7242576590f51ed825aa9ea4b8f867b250842e15
ankudinov <petr.ankudinov@gmail.com>
2023-05-03 13:22:17 +0000

    add regex option to file search

--- runAM/tools/find.py
+++ runAM/tools/find.py
@@ -1,27 +1,36 @@
 import os
 import sys
+import re
 
-def file_in_dir_and_subdirs(dirname, target_filename):
+def file_in_dir_and_subdirs(dirname, target_filename, regex = False):
     """Find file in the specified directory and all subdirectories.
 
     Args:
         dirname (str): Directory name to start search
         target_filename (str): Target filename
+        regex (bool, optional): Match target filename as regex. Defaults to False.
 
     Returns:
         list: Full path list matching the specified file name.
     """
     filename_list = list()
+    # TODO: make this code block less repetitive
     for dirpath, _, filenames in os.walk(dirname):
-        for filename in [f for f in filenames if f == target_filename]:
-            filename_list.append(
-                os.path.abspath(os.path.join(dirpath, filename))
-            )
+        if regex:
+            for filename in [f for f in filenames if re.search(r'{}'.format(target_filename), f)]:
+                filename_list.append(
+                    os.path.abspath(os.path.join(dirpath, filename))
+                )
+        else:
+            for filename in [f for f in filenames if f == target_filename]:
+                filename_list.append(
+                    os.path.abspath(os.path.join(dirpath, filename))
+                )
 
     return filename_list
 
 
-def file_full_path(filename, dirname = '', single_match = True):
+def file_full_path(filename, dirname = '', single_match = True, regex = False):
     """This function takes a short filename and optional prefix and returns full path.
 
     Args:
@@ -29,6 +38,7 @@ def file_full_path(filename, dirname = '', single_match = True):
         dirname (str, optional): The directory name to search for a file. Defaults to ''.
             By default the file search will start in the current working directory.
         single_match (bool, optional): If true, returns as single filename, otherwise list of matching files. Defaults to True.
+        regex (bool, optional): Match target filename as regex. Defaults to False.
 
     Returns:
         list: Full path list matching the specified filename. By default must be a single full path.
@@ -39,7 +49,7 @@ def file_full_path(filename, dirname = '', single_match = True):
     else:
         search_directory = os.getcwd()
 
-    full_path_list = file_in_dir_and_subdirs(search_directory, filename)
+    full_path_list = file_in_dir_and_subdirs(search_directory, filename, regex)
 
 
     if len(full_path_list) == 0:
