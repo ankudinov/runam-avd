82565f8be7077379c2b0f224f15aa1ad6f22546a
ankudinov <petr.ankudinov@gmail.com>
2023-05-03 12:17:17 +0000

    add csv.read

--- runAM/json.py
+++ runAM/json.py
@@ -4,7 +4,7 @@ import runAM.tools.find
 
 def read(json_file_name):
     try:
-        with open(runAM.tools.find.file_full_path(json_file_name)[0]) as f:
-            return json.load(f)
+        with open(runAM.tools.find.file_full_path(json_file_name)[0], mode='r') as json_file:
+            return json.load(json_file)
     except Exception as e:
         sys.exit(f'ERROR: Can not load the JSON file {json_file_name} due to following error:\n{e}')
