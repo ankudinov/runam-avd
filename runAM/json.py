45da7cd61d235b1b5afa08fa2f8608d784c4ac20
ankudinov <petr.ankudinov@gmail.com>
2023-05-03 11:40:46 +0000

    fix path to modules

--- runAM/json.py
+++ runAM/json.py
@@ -1,10 +1,10 @@
 import json
 import sys
-from runAM.tools.find import file_full_path
+import runAM.tools.find
 
 def read(json_file_name):
     try:
-        with open(file_full_path(json_file_name)[0]) as f:
+        with open(runAM.tools.find.file_full_path(json_file_name)[0]) as f:
             return json.load(f)
     except Exception as e:
         sys.exit(f'ERROR: Can not load the JSON file {json_file_name} due to following error:\n{e}')
